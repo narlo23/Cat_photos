da047dd6f280ea695e7b974545ffd6a3b5287447
Hyojin Ahn <77222481+narlo23@users.noreply.github.com>
2023-01-10 17:29:07 +0900

    Add files via upload

---
+++ Cat_Photos/src/App.js
@@ -0,0 +1,38 @@
+import { request } from "./api.js";
+import Nodes from "./Nodes.js";
+
+export default function App({ $target }) {
+  this.state = {
+    isRoot: true,
+    nodes: [],
+  };
+  const nodes = new Nodes({
+    $target,
+    initialState: {
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes,
+    },
+    onClick: () => {},
+  });
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+
+    nodes.setState({
+      isRoot: this.state.isRoot,
+      nodes: this.state.nodes,
+    });
+  };
+
+  const fetchNodes = async (id) => {
+    const nodes = await request(id ? `/${id}` : "/");
+
+    this.setState({
+      ...this.state,
+      nodes,
+      isRoot: id ? false : true,
+    });
+  };
+
+  fetchNodes();
+}
