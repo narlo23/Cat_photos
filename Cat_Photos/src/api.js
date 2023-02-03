f5a0bfe2341384abc2db808716db23bb28a652a6
narlo <aks152312@gmail.com>
2023-02-03 13:07:46 +0900

    api 경로 수정

--- Cat_Photos/src/api.js
+++ Cat_Photos/src/api.js
@@ -1,4 +1,4 @@
-const API_END_POINT = "https://mwu.roto-cat-api.programmers.co.kr";
+const API_END_POINT = "https://cat-photos.edu-api.programmers.co.kr";
 export const request = async (url) => {
   try {
     const res = await fetch(`${API_END_POINT}${url}`);
